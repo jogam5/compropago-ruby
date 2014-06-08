585a1b400eb73567f09619db98fd2f2e984465ee
jogam5 <jogam5@yahoo.com.mx>
2014-06-08 18:20:36 -0500

    first commit

---
+++ lib/compropago.rb
@@ -0,0 +1,18 @@
+# API spec at http://compropago.com/documentacion/api
+require 'net/https'
+require 'uri'
+
+# Version
+require 'compropago/version'
+
+# API operations
+require 'compropago/api_operations/create'
+
+# Resources
+require 'compropago/client'
+
+module Compropago
+  # Your code goes here...
+  #@api_base = 'https://api.compropago.com'
+
+end
---
+++ lib/compropago/client.rb
@@ -0,0 +1,34 @@
+require 'net/https'
+require 'uri'
+
+module ComproPago
+  class Client
+
+  	BASE_URI = 'https://api.compropago.com/v1'
+
+  	def initialize(api_key='', options={})
+  	  @api_key = api_key
+
+  	  #defaults
+  	  options[:base_uri] ||= BASE_URI
+  	  @base_uri = options[:base_uri]	
+  	end
+
+  	def create_charge product_price, product_name, customer_name, customer_email, payment_type
+  	  uri = URI.parse(BASE_URI+'/charges')
+  	  http = Net::HTTP.new(uri.host, uri.port)
+  	  http.use_ssl = true
+  	  request = Net::HTTP::Post.new(uri.request_uri)
+  	  request.basic_auth @api_key, ''
+  	  params = { "product_price" => product_price,
+  	  			 "product_name" => product_name,
+  	  			 "customer_name" => customer_name,
+  	  			 "customer_email" => customer_email,
+  	  			 "payment_type" => payment_type
+  	  		  }
+  	  request.set_form_data(params)
+  	  response = http.request(request)
+  	  puts response.body
+  	end
+  end
+end
